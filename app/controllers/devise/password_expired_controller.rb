e66191e30f1146896d420193136a48abd916f9e4
mobilaizeris <mobilaizeris@gmail.com>
2018-06-21 18:13:40 +0300

    Update password_expired_controller.rb

--- app/controllers/devise/password_expired_controller.rb
+++ app/controllers/devise/password_expired_controller.rb
@@ -1,41 +1,36 @@
 class Devise::PasswordExpiredController < DeviseController
-  skip_before_action :handle_password_change
-  before_action :skip_password_change, only: [:show, :update]
-  prepend_before_action :authenticate_scope!, :only => [:show, :update]
+  skip_before_filter :handle_password_change
+  prepend_before_filter :authenticate_scope!, :only => [:show, :update]
 
   def show
-    respond_with(resource)
+    if not resource.nil? and resource.need_change_password?
+      respond_with(resource)
+    else
+      redirect_to :root
+    end
   end
 
   def update
-    resource.extend(Devise::Models::DatabaseAuthenticatablePatch)
-    if resource.update_with_password(resource_params)
-      warden.session(scope)['password_expired'] = false
-      set_flash_message :notice, :updated
-      bypass_sign_in resource, scope: scope
-      redirect_to stored_location_for(scope) || :root
+    if resource
+      resource.extend(Devise::Models::DatabaseAuthenticatablePatch)
+      if resource.update_with_password(resource_params)
+        warden.session(scope)['password_expired'] = false
+        set_flash_message :notice, :updated
+        sign_in scope, resource, :bypass => true
+        redirect_to stored_location_for(scope) || :root
+      else
+        clean_up_passwords(resource)
+        respond_with(resource, action: :show)
+      end
     else
-      clean_up_passwords(resource)
-      respond_with(resource, action: :show)
+      redirect_to :root
     end
   end
 
   private
-
-  def skip_password_change
-    return if !resource.nil? && resource.need_change_password?
-    redirect_to :root
-  end
-
-  def resource_params
-    permitted_params = [:current_password, :password, :password_confirmation]
-
-    if params.respond_to?(:permit)
-      params.require(resource_name).permit(*permitted_params)
-    else
-      params[scope].slice(*permitted_params)
+    def resource_params
+      params.require(resource_name).permit(:current_password, :password, :password_confirmation)
     end
-  end
 
   def scope
     resource_name.to_sym
